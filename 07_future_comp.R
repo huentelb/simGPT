63d0483549ef8c22b03dc603441d503679727596
Bettina <huenteler@wiso.uni-koeln.de>
2025-02-03 20:47:14 +0100

    Create 07_future_comp.R
    Code for comparing 1953 and 2000 cohorts

---
+++ 07_future_comp.R
@@ -0,0 +1,213 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# COMPARISON OF SEQUENCES ACROSS COHORTS 
+# 1953 & 2000 BIRTH COHORT - AGE RANGE 0-100
+
+# huenteler@demogr.mpg.de
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+library(janitor) # for rownames_to_column
+
+max_age <- 100
+ages <- as.character(c(0:max_age))
+lab_ages <- paste0("age", ages)
+
+# Folder based on simulation base_seed
+folder.baseseed <- paste0(folder,"/sim_results_", supfile, "_",base_seed,"_/")
+if (!dir.exists(folder.baseseed)) {
+  # If not, create the new folder
+  dir.create(folder.baseseed)
+  cat("Folder created:", folder.baseseed, "\n")
+} else {
+  cat("Folder already exists:", folder.baseseed, "\n")
+}
+
+# Laod gp data of both cohorts and store as separate dataframes with according suffix
+load(paste0(folder.baseseed, "gp1953100.RData"))
+gp53 <- gp
+load(paste0(folder.baseseed, "gp2000100.RData"))
+gp00 <- gp
+
+
+### AGGREGATE-LEVEL COMPARISONS ####
+
+agg <- gp53 %>% 
+  rbind(gp00) %>% 
+  select(dob_year, dage, pdage, isparent, numkids, cage, isgparent, numgkids, gcage) %>% 
+  group_by(dob_year) %>% 
+  summarise_all(mean, na.rm = TRUE) %>% 
+  mutate(cohort = dob_year) %>% 
+  select(cohort, dage:gcage) # exchange 'cohort' for 'dob_year'
+
+
+# Swap rows and columns of indic_mean
+tab2_agg <- agg %>%
+  mutate(across(-cohort, ~ round(.x, 2))) %>%
+  pivot_longer(-cohort, names_to = "variable", values_to = "value") %>%
+  pivot_wider(names_from = cohort, values_from = value) %>%
+  column_to_rownames("variable") # Moves "variable" column to row names
+
+# Number of observations
+tab2_n <- gp53 %>%
+  rbind(gp00) %>% 
+  mutate(cohort = dob_year) %>% 
+  count(cohort) %>% 
+  pivot_wider(names_from = cohort, values_from = n)
+
+rownames(tab2_n) <- "N"
+
+
+### DEFINE SEQUENCES ####
+# Define characteristics of sequences
+
+# Alphabet, ie all possible states
+gpalpha <- c("P1C0G0", "P0C0G0", 
+             "P1C1G0", "P0C1G0", 
+             "P1C1G1", "P0C1G1")
+
+# Labels of states
+gplabels <- c("child", "no ancestors/descentants",
+              "child and parent", "parent",
+              "child, parent, and grandparent", 
+              "parent and grandparent")
+
+# Short labels for states
+gpstates <-c("G1G2", "G2",
+             "G1G2G3", "G2G3",
+             "G1G2G3G4", "G2G3G4")
+
+# Color palette
+cblind <- brewer.pal(6, "RdBu")
+
+
+# Define sequence object using the above characteristics
+seq53 <- seqdef(gp53, 6:paste0(max_age+6), # for max_age 100 to column 106, for max_age 66 to column 72
+              labels = gplabels,  
+              cnames = ages, 
+              tick.last = TRUE, 
+              xtstep = 5, 
+              cpal = cblind, 
+              alphabet = gpalpha, 
+              states = gpstates,
+              missing = "D", right = "DEL")
+
+seq00 <- seqdef(gp00, 6:paste0(max_age+6), # for max_age 100 to column 106, for max_age 66 to column 72
+                labels = gplabels,  
+                cnames = ages, 
+                tick.last = TRUE, 
+                xtstep = 5, 
+                cpal = cblind, 
+                alphabet = gpalpha, 
+                states = gpstates,
+                missing = "D", right = "DEL")
+
+
+
+### COMPARISON OF SEQUENCES ####
+
+#### Sequence indicators ####
+
+# "taking nonvisited states into account -> variance as a predictability indicator of the state duration"
+# "ignoring nonvisited states -> measuring the variance of the observed spells." (Ritschard 2023: 2045)
+
+# in our case: number of spells = number of visited states; whenever a new spell begins, a new state is visited
+# as one cannot go back to a state that they left (parents are not reborn; in our setting, (grand)children do not die)
+# -> recurrence meaningless
+
+indic53 <- seqindic(seq53, indic=c("lgth", "visited", "visitp", "transp", # length, states visited + prop, no. transitions
+                               "entr", "meand", "dustd", # mean spell duration + SD (accounting for non-visited states: , "meand2", "dustd2")
+                               "cplx"), with.missing=F) # complexity index
+
+indic00 <- seqindic(seq00, indic=c("lgth", "visited", "visitp", "transp", # length, states visited + prop, no. transitions
+                                   "entr", "meand", "dustd", # mean spell duration + SD (accounting for non-visited states: , "meand2", "dustd2")
+                                   "cplx"), with.missing=F) # complexity index
+
+
+# store mean across full sample
+indic_mean53 <- indic53 %>%
+  summarise(round(across(everything(), \(x) mean(x, na.rm = TRUE)),2)) %>% 
+  mutate(cohort = 1953) %>% 
+  select(-Visited) %>% 
+  select(cohort, Lgth:Cplx)
+
+indic_mean00 <- indic00 %>%
+  summarise(round(across(everything(), \(x) mean(x, na.rm = TRUE)),2)) %>% 
+  mutate(cohort = 2000) %>% 
+  select(-Visited) %>% 
+  select(cohort, Lgth:Cplx)
+
+indic_mean <- indic_mean53 %>% 
+  rbind(indic_mean00) 
+
+# Swap rows and columns of indic_mean
+tab2_ind <- indic_mean %>%
+  pivot_longer(-cohort, names_to = "variable", values_to = "value") %>%
+  pivot_wider(names_from = cohort, values_from = value) %>%
+  column_to_rownames("variable")
+
+
+
+#### BIC & LRT ####
+
+# Compare occurrence of states
+comp_occ <- round(seqCompare(seq53, seqdata2 = seq00, stat = "all", 
+                             method = "OMspell", sm = "INDELS", indel = 2, expcost = 0.5),
+                  2)
+
+# Compare timing of statesgp$
+comp_time <- round(seqCompare(seq53, seqdata2 = seq00, stat = "all", 
+                              method = "CHI2", step = 101),
+                   2)
+
+# Compare duration in statesgp$
+comp_dur <- round(seqCompare(seq53, seqdata2 = seq00, stat = "all", 
+                             method = "OMstran", otto = 0.5, sm = "INDELSLOG"),
+                  2)
+
+# Combine all BIC and LRT into one df
+bic <- as.data.frame(comp_occ, row.names = "Occurence") %>% 
+  rbind(as.data.frame(comp_time, row.names = "Timing"),
+        as.data.frame(comp_dur, row.names = "Duration")) %>% 
+  select(3,1,2) # select and order columns (BIC, LRT, p-value) 
+
+# Swap rows and columns of bic?
+
+
+#### AVERAGE GP MEAN TIME ####
+gp_mt53 <- round(seqmeant(seq53),2) #, serr = T for calculating Var, SD, SE
+gp_mt00 <- round(seqmeant(seq00),2)
+
+colnames(gp_mt53) <- "1953"
+colnames(gp_mt00) <- "2000"
+
+tab2_mt <- gp_mt53 %>% 
+  cbind(gp_mt00) %>% 
+  as.data.frame() 
+
+
+#### CREATE TAB 2 ####
+tab2 <- tab2_agg %>% 
+  rbind(tab2_n, tab2_mt, tab2_ind)
+
+tab2 <- rownames_to_column(tab2, var = "Measure")
+
+library(gt)
+library(flextable)
+
+set_flextable_defaults(
+  font.size = 11,
+  border.color = 'black',
+  line_spacing = 1.3,
+)
+
+ft2 <- flextable(tab2) %>% 
+  add_header_row(colwidths = c(1,2), values = c(" ", "Cohort")) %>% 
+  align(align = "center", part = "header") %>% 
+  align(j = 1, align = "left", part = "body") # first column left-align
+ft2
+
+save_as_docx("Table 2" = ft2, path = paste0(folder.baseseed, "Tab2.docx"), align = "left")
+
+### last line ###
