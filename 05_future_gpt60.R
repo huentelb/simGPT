985cc280240cc941132250be63bbdc30432e0585
Bettina Hünteler <huenteler@demogr.mpg.de>
2026-03-12 13:24:22 +0100

    Change order of files
    * adjust merging of single simulations accordingly

--- 05_future_gpt60.R
+++ 05_future_gpt60.R
@@ -28,17 +28,28 @@ cohort <- 1960
 max_age <- 100
 ages <- as.character(c(0:max_age))
 
-# Generate folders to store results
 
-# 1. Upper level folder based on simulation base_seed
-folder.baseseed <- paste0(folder,"/sim_results_", base_seed,"_/")
-if (!dir.exists(folder.baseseed)) {
-  # If not, create the new folder
-  dir.create(folder.baseseed)
-  cat("Folder created:", folder.baseseed, "\n")
-} else {
-  cat("Folder already exists:", folder.baseseed, "\n")
+
+# load the gp-data for from each simulation from each birth cohorts
+for (c in c(1960, 2000)) {
+  
+  for (i in c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) {
+    # load(paste0(folder, "/sim_results_", supfile, "_",base_seed,i,"_/gp", c, max_age, ".RData"))
+    
+    load(paste0(folder, "/sim_results_", base_seed,i,"_/gp", c, max_age, ".RData"))
+    assign(paste0("gp_", i), gp)  
+  }
+  
+  # merge the simulations into one gp-dataframe for each birth cohort
+  gp <- rbind(gp_1, gp_2, gp_3, gp_4, gp_5, gp_6, gp_7, gp_8, gp_9, gp_10)
+  
+  
+  # store combined gp dataframe in base_seed folder
+  save(gp, file = paste0(folder.baseseed, "gp", c, max_age, ".RData"))
+  
 }
+
+
 
 # 2. Lower level folder for cohort-max age-specific output 
 graph.folder <- paste0(folder.baseseed, cohort, "_", max_age, "/")
